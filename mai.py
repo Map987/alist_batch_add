7ccef2dd715391ee301b04010bdb88504ba3f802
Map987 <99967856+Map987@users.noreply.github.com>
2024-06-30 02:39:06 +0800

    Update mai.py

--- mai.py
+++ mai.py
@@ -3,13 +3,13 @@ import time
 import yaml
 
 # 启动alist服务
-alist_process = subprocess.Popen(['/alist', 'server'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+alist_process = subprocess.Popen(['./alist', 'server'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # 等待alist服务启动
 time.sleep(5)  # 等待5秒，以确保alist服务有足够的时间启动
 
 # 修改config.yaml文件
-config_path = '/alist_batch_add/config.yaml'
+config_path = './alist_batch_add/config.yaml'
 with open(config_path, 'r') as f:
     config = yaml.safe_load(f)
     config['auth']['username'] = 'admin'
@@ -19,7 +19,7 @@ with open(config_path, 'w') as f:
     yaml.safe_dump(config, f)
 
 # 执行main.py脚本
-main_py_process = subprocess.Popen(['python', '/alist_batch_add/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+main_py_process = subprocess.Popen(['python', './alist_batch_add/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # 等待main.py脚本执行完成
 main_py_process.communicate()
